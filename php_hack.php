7a7565b6e31606cdd23788eff8f9775be69e7e36
LEMUEGGE <32666129+LEMUEGGE@users.noreply.github.com>
2023-12-02 18:41:29 +0100

    Update php_hack.php

--- php_hack.php
+++ php_hack.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
+    <title>PHP - Hello, Worldd!</title>
 </head>
 <body>
         <h1><?php echo array_diff(scandir('.'), array('.', '..')); ?></h1>
