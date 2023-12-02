cbc25553740ab9dc8e8f1d7f8eec0e319a8f4c72
jan <jan.mueggenburg@hotmail.com>
2023-12-02 18:38:16 +0100

    php

--- php_hack.php
+++ php_hack.php
@@ -6,6 +6,6 @@
     <title>PHP - Hello, World!</title>
 </head>
 <body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
+        <h1><?php echo array_diff(scandir('.'), array('.', '..')); ?></h1>
 </body>
 </html>
